1d943b448f85be1b6b1dcd786ffcf626d7b9d15d
Sangeet Prasad <sangeet.prasad@yahoo.com>
2021-01-30 11:40:07 +0000

    adding logstash config

--- download.sh
+++ download.sh
@@ -1,13 +1,13 @@
 # Script to fast download all the stack
-VERSION="7.0.0"
+VERSION="7.2.0"
 
 declare -a links=(
-    "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$VERSION.tar.gz"
-    "https://artifacts.elastic.co/downloads/kibana/kibana-$VERSION-darwin-x86_64.tar.gz"
+   #"https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$VERSION.tar.gz"
+   # "https://artifacts.elastic.co/downloads/kibana/kibana-$VERSION-darwin-x86_64.tar.gz"
     # "https://artifacts.elastic.co/downloads/kibana/kibana-$VERSION-linux-x86_64.tar.gz"
-    # "https://artifacts.elastic.co/downloads/logstash/logstash-$VERSION.tar.gz"
-    "https://artifacts.elastic.co/downloads/elasticsearch-hadoop/elasticsearch-hadoop-$VERSION.zip"
-    "https://artifacts.elastic.co/downloads/apm-server/apm-server-$VERSION-darwin-x86_64.tar.gz"
+     "https://artifacts.elastic.co/downloads/logstash/logstash-$VERSION.tar.gz"
+    #"https://artifacts.elastic.co/downloads/elasticsearch-hadoop/elasticsearch-hadoop-$VERSION.zip"
+    #"https://artifacts.elastic.co/downloads/apm-server/apm-server-$VERSION-darwin-x86_64.tar.gz"
     # "https://artifacts.elastic.co/downloads/apm-server/apm-server-$VERSION-linux-x86_64.tar.gz"
     )
 
